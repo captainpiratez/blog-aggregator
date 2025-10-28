b2aaa8b564646bced78419e0f685fa99c2b463dd
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2025-10-28 12:29:37 +0300

    Add Post model and database query

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -10,6 +10,11 @@ SELECT * FROM feeds;
 SELECT * FROM feeds
 WHERE url = $1;
 
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
+
 -- name: MarkFeedFetched :one
 UPDATE feeds
 SET last_fetched_at = NOW(),
